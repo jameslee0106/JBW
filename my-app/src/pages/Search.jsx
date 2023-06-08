b0e3c4ed5c14626288f5279999679dc81947fb3b
jameslee0106 <leejames0106@gmail.com>
2023-06-07 21:15:50 -0700

    additional ui changes

--- my-app/src/pages/Search.jsx
+++ my-app/src/pages/Search.jsx
@@ -5,7 +5,25 @@ function Search() {
   return (
     <div>
     <Navbar />
-    <h1 className="text-white">Search</h1>
+    <div className='relative w-full h-screen bg-zinc-900/90'>
+
+    <div className='flex justify-center items-center h-full'>
+      <form className='max-w-[400px] w-full mx-auto bg-white p-8 rounded-lg'>
+        <h2 className='text-4xl font-bold text-center py-8'>Job Search</h2>
+        <div className='flex flex-col mb-4'>
+          <label>Enter Occupation</label>
+          <input className='border relative bg-gray-100 p-2' type="email"/>
+        </div>
+        <div className='flex flex-col mb-4'>
+          <label>Enter Zip Code</label>
+          <input className='border relative bg-gray-100 p-2' type="password"/>
+        </div>
+        <button className='w-full py-3 mt-8 bg-purple-700 hover:bg-purple-800 relative text-white rounded-lg font-semibold'>
+          Search Now
+        </button>
+      </form>
+    </div>
+    </div>
   </div>
   )
 }
